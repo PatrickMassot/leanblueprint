7589020c6a51ecef3735081002abe6ff4e1130fc
Patrick Massot <patrickmassot@free.fr>
2020-05-29 00:25:32 +0200

    Work on proof graphs

--- leanblueprint/Packages/blueprint.py
+++ leanblueprint/Packages/blueprint.py
@@ -27,12 +27,12 @@ showmore: enable buttons showing or hiding proofs.
 import os
 import string
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 from jinja2 import Template
 from pygraphviz import AGraph
 
-from plasTeX import Command
+from plasTeX import Command, Environment, TeXDocument
 from plasTeX.PackageResource import (
         PackageTemplateDir, PackageJs, PackageCss, PackagePreCleanupCB)
 
@@ -54,6 +54,7 @@ class DepGraph():
     def __init__(self):
         self.nodes = set()
         self.edges = set()
+        self.proof_edges = set()
 
     def to_dot(self, shapes: dict()) -> AGraph:
         graph = AGraph(directed=True, bgcolor='#e8e8e8')
@@ -61,6 +62,8 @@ class DepGraph():
             graph.add_node(node.id, shape=shapes.get(item_kind(node), 'ellipse'))
         for s, t in self.edges:
             graph.add_edge(s.id, t.id)
+        for s, t in self.proof_edges:
+            graph.add_edge(s.id, t.id, style='dashed')
         return graph
 
 class uses(Command):
@@ -75,12 +78,6 @@ class uses(Command):
             labels_dict = doc.context.labels
             used = [labels_dict[label] for label in self.attributes['labels'] if label in labels_dict]
             node.setUserData('uses', used)
-            if 'blueprint_dep_graph' in doc.userdata:
-                graph = doc.userdata.get('blueprint_dep_graph')
-                graph.nodes.add(node)
-                graph.nodes.update(used)
-                for thm in used:
-                    graph.edges.add((thm, node))
 
         doc.postParseCallbacks.append(update_used)
 
@@ -92,7 +89,7 @@ class proves(Command):
         Command.digest(self, tokens)
         node = self.parentNode
         doc = self.ownerDocument
-        def update_proved():
+        def update_proved() -> None:
             labels_dict = doc.context.labels
             proved = labels_dict.get(self.attributes['label'])
             if proved:
@@ -100,6 +97,51 @@ class proves(Command):
                 proved.userdata['proved_by'] = node
         doc.postParseCallbacks.append(update_proved)
 
+class collectproofs(Command):
+    """Collects all proofs and try to link to their statement"""
+    # FIXME: We define a command here because we are missing a hook
+    # to call a function after everything is parsed but before rendering
+    # This command should be at the end of the document
+    # Could we hook that into AtDocumentEnd?
+
+    def digest(self, tokens):
+        Command.digest(self, tokens)
+        doc = self.ownerDocument
+        def update_proofs() -> None:
+            for proof in doc.getElementsByTagName('proof'):
+                proved = proof.userdata.setdefault('proves', find_proved_thm(proof))
+                if proved:
+                    proved.userdata['proved_by'] = proof
+        doc.postParseCallbacks.append(update_proofs)
+
+class makegraph(Command):
+    """Make the dep graph"""
+    # FIXME: We define a command here because we are missing a hook
+    # to call a function after everything is parsed but before rendering
+    # This command should be at the end of the document
+    # Could we hook that into AtDocumentEnd?
+
+    def digest(self, tokens):
+        Command.digest(self, tokens)
+        doc = self.ownerDocument
+        def makeit() -> None:
+            nodes = []
+            for thm_type in doc.userdata['thm_types']:
+                nodes += doc.getElementsByTagName(thm_type)
+            graph = doc.userdata.get('blueprint_dep_graph')
+            for node in nodes:
+                graph.nodes.add(node)
+                used = node.userdata.get('uses', [])
+                #graph.nodes.update(used)
+                for thm in used:
+                    graph.edges.add((thm, node))
+                proof = node.userdata.get('proved_by')
+                if proof:
+                    used = proof.userdata.get('uses', [])
+                    for thm in used:
+                        graph.proof_edges.add((thm, node))
+
+        doc.postParseCallbacks.append(makeit)
 
 class lean(Command):
     r"""\lean{decl list} """
@@ -173,6 +215,17 @@ class Report():
         self.nb_not_covered = sum([p.nb_not_covered for p in partials])
         self.coverage = 100 * (self.nb_thms - self.nb_not_covered) / self.nb_thms if self.nb_thms else 100
 
+def find_proved_thm(proof) -> Optional[Environment]:
+    """From a proof node, try to find the statement."""
+    node = proof.parentNode
+    while node.previousSibling:
+        childNodes = node.previousSibling.childNodes
+        if childNodes and childNodes[0].nodeName == 'thmenv':
+            return childNodes[0]
+        node = node.previousSibling
+    return None
+
+
 
 
 def ProcessOptions(options, document):
@@ -193,86 +246,87 @@ def ProcessOptions(options, document):
 
     package_option['links'] = 'usage_links' in options
 
-    if 'dep_graph' in options:
-        d3_url = options.get('d3_url', 'https://d3js.org/d3.v5.min.js')
-        jquery_url = options.get('jquery_url', 'http://code.jquery.com/jquery.min.js')
-        title = options.get('title', 'Dependencies')
-        document.userdata['blueprint_dep_graph'] = DepGraph()
-        graph_target = options.get( 'dep_graph_target', 'dep_graph.html')
+    ## Dep graph
+    d3_url = options.get('d3_url', 'https://d3js.org/d3.v5.min.js')
+    jquery_url = options.get('jquery_url', 'http://code.jquery.com/jquery.min.js')
+    title = options.get('title', 'Dependencies')
+    document.userdata['blueprint_dep_graph'] = DepGraph()
+    graph_target = options.get( 'dep_graph_target', 'dep_graph.html')
 
-        default_tpl_path = PKG_DIR.parent/'templates'/'dep_graph.j2'
-        graph_tpl_path = Path(options.get('dep_graph_tpl', default_tpl_path))
-        try:
-            graph_tpl = Template(graph_tpl_path.read_text())
-        except IOError:
-            log.warning('DepGraph template read error, using default template')
-            graph_tpl = Template(default_tpl_path.read_text())
+    default_tpl_path = PKG_DIR.parent/'templates'/'dep_graph.j2'
+    graph_tpl_path = Path(options.get('dep_graph_tpl', default_tpl_path))
+    try:
+        graph_tpl = Template(graph_tpl_path.read_text())
+    except IOError:
+        log.warning('DepGraph template read error, using default template')
+        graph_tpl = Template(default_tpl_path.read_text())
 
-        def makeDepGraph(document):
-            graph = document.userdata['blueprint_dep_graph']
-            graph.to_dot({'definition': 'box'}).write('graph.dot')
-            graph_tpl.stream(
-                    graph=graph,
-                    context=document.context,
-                    d3_url=d3_url,
-                    jquery_url=jquery_url,
-                    title=title,
-                    config=document.config).dump(graph_target)
-            return [graph_target]
+    def makeDepGraph(document):
+        graph = document.userdata['blueprint_dep_graph']
+        graph.to_dot({'definition': 'box'}).write('graph.dot')
+        graph_tpl.stream(
+                graph=graph,
+                context=document.context,
+                d3_url=d3_url,
+                jquery_url=jquery_url,
+                title=title,
+                config=document.config).dump(graph_target)
+        return [graph_target]
 
-        cb = PackagePreCleanupCB(
-                renderers=['html5'],
-                data=makeDepGraph)
-        # FIXME: All those resources are included in all pages. Need a flag in
-        # PackageResource to copy only without adding to the rendererdata
-        css = PackageCss(
-                renderers=['html5'],
-                path=STATIC_DIR/'dep_graph.css')
-        js = PackageJs(
-                renderers=['html5'],
-                path=STATIC_DIR/'d3-graphviz.js')
-        js2 = PackageJs(
-                renderers=['html5'],
-                path=STATIC_DIR/'dep_graph.js')
-        document.addPackageResource([cb, css, js, js2])
+    cb = PackagePreCleanupCB(
+            renderers=['html5'],
+            data=makeDepGraph)
+    # FIXME: All those resources are included in all pages. Need a flag in
+    # PackageResource to copy only without adding to the rendererdata
+    css = PackageCss(
+            renderers=['html5'],
+            path=STATIC_DIR/'dep_graph.css')
+    js = PackageJs(
+            renderers=['html5'],
+            path=STATIC_DIR/'d3-graphviz.js')
+    js2 = PackageJs(
+            renderers=['html5'],
+            path=STATIC_DIR/'dep_graph.js')
+    document.addPackageResource([cb, css, js, js2])
 
-    if 'coverage' in options:
-        default_tpl_path = PKG_DIR.parent/'templates'/'coverage.j2'
-        cov_tpl_path = options.get( 'coverage_tpl', default_tpl_path)
-        try:
-            cov_tpl = Template(cov_tpl_path.read_text())
-        except IOError:
-            log.warning('Coverage template read error, using default template')
-            cov_tpl = Template(default_tpl_path.read_text())
+    ## Coverage
+    default_tpl_path = PKG_DIR.parent/'templates'/'coverage.j2'
+    cov_tpl_path = options.get( 'coverage_tpl', default_tpl_path)
+    try:
+        cov_tpl = Template(cov_tpl_path.read_text())
+    except IOError:
+        log.warning('Coverage template read error, using default template')
+        cov_tpl = Template(default_tpl_path.read_text())
 
 
-        coverage_target = options.get( 'coverage_target', 'coverage.html')
-        outfile = os.path.join(outdir, coverage_target)
+    coverage_target = options.get( 'coverage_target', 'coverage.html')
+    outfile = os.path.join(outdir, coverage_target)
 
-        thm_types = [thm.strip()
-                for thm in options.get('coverage_thms',
-                    'definition+lemma+proposition+theorem').split('+')]
-        section = options.get('coverage_sectioning', 'chapter')
+    thm_types = [thm.strip()
+            for thm in options.get('coverage_thms',
+                'definition+lemma+proposition+theorem').split('+')]
+    document.userdata['thm_types'] = thm_types
+    section = options.get('coverage_sectioning', 'chapter')
 
-        def makeCoverageReport(document):
-            sections = document.getElementsByTagName(section)
-            report = Report([PartialReport.from_section(sec, thm_types) for sec in sections])
-            cov_tpl.stream(
-                    report=report,
-                    config=document.config,
-                    terms=document.context.terms).dump(outfile)
-            return [outfile]
+    def makeCoverageReport(document):
+        sections = document.getElementsByTagName(section)
+        report = Report([PartialReport.from_section(sec, thm_types) for sec in sections])
+        cov_tpl.stream(
+                report=report,
+                config=document.config,
+                terms=document.context.terms).dump(outfile)
+        return [outfile]
 
-        cb = PackagePreCleanupCB(
-                renderers=['html5'],
-                data=makeCoverageReport)
-        css = PackageCss(
-                renderers=['html5'],
-                path=STATIC_DIR/'style_coverage.css')
-        js = PackageJs(
-                renderers=['html5'],
-                path=STATIC_DIR/'coverage.js')
-        document.addPackageResource([cb, css, js])
+    cb = PackagePreCleanupCB(
+            renderers=['html5'],
+            data=makeCoverageReport)
+    css = PackageCss(
+            renderers=['html5'],
+            path=STATIC_DIR/'style_coverage.css')
+    js = PackageJs(
+            renderers=['html5'],
+            path=STATIC_DIR/'coverage.js')
+    document.addPackageResource([cb, css, js])
 
     if 'showmore' in options:
         navs = [{'icon': 'eye-minus', 'id': 'showmore-minus', 'class': 'showmore'},
