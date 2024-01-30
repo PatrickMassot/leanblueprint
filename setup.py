bcf21cd5ddfe2ea20b7ef28adc5bcd77cd655d9c
Patrick Massot <patrickmassot@free.fr>
2024-01-30 18:25:56 -0500

    Modern packaging

--- setup.py
+++ setup.py
@@ -1,22 +1,5 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="leanblueprint",
-    version="0.0.2",
-    author="Patrick Massot",
-    description="Lean prover blueprint plasTeX plugin.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    package_data={'leanblueprint': ['static/*', 'Packages/*',
-                                    'Packages/renderer_templates/*']},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent"],
-    python_requires='>=3.5',
-    install_requires=['plasTeX>=3.0', 'plastexdepgraph', 'plastexshowmore']
-)
+if __name__ == "__main__":
+    setup()
